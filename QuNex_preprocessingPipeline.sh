4fc4dc27b3bb2b1c3536dd74263d5de54a282194
Manesh Girn <46899986+mgirn@users.noreply.github.com>
2025-04-04 17:06:37 -0700

    Add files via upload

---
+++ QuNex_preprocessingPipeline.sh
@@ -0,0 +1,3 @@
+The pipeline used by UZH that is described in the manuscript as the “QuNex pipeline” used the QuNex preprocessing software suite found here: https://qunex.yale.edu/
+
+The above webpage provides extensive documentation on how to reproduce this pipeline.
